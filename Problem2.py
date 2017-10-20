c250a03b20823cb069eab16357b882a8b3cb2ce6
yharaskrik <jaycooperbell@gmail.com>
2017-10-20 16:52:40 -0700

    Solved Problem 2, even fibonacci numbers With constraint of max value

--- Problem2.py
+++ Problem2.py
@@ -1,2 +1,3 @@
 from functools import reduce
-print(list(filter(lambda x: x % 2 == 0, ((lambda n: reduce(lambda y, _: y+[y[-1] + y[-2]], range(n - 2), [0, 1]))(10)))))
+import math
+print(sum(list(filter(lambda x: (x % 2 == 0 and x < 4000000), ((lambda n: reduce(lambda y, _: y+[y[-1] + y[-2]], range(int(math.ceil(math.log10(n * math.sqrt(5)) / math.log10((1 + math.sqrt(5)) / 2))) - 2), [0, 1]))(4000000))))))
