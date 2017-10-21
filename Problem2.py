49ad701f25b4aec7fe2616035d3c6b50722a0f06
yharaskrik <jaycooperbell@gmail.com>
2017-10-20 17:29:13 -0700

    Added in timing to run 100,000 times and average times without extra variable declaration and prints

--- Problem2.py
+++ Problem2.py
@@ -1,9 +1,11 @@
 from functools import reduce
 import math
 import time
-millis = time.time()
-print(millis)
-print(sum(list(filter(lambda x: (x % 2 == 0 and x < 4000000), ((lambda n: reduce(lambda y, _: y+[y[-1] + y[-2]], range(int(math.ceil(math.log10(n * math.sqrt(5)) / math.log10((1 + math.sqrt(5)) / 2))) - 2), [0, 1]))(4000000))))))
-end = time.time()
-print(end)
-print(end - millis)
+total_millis = 0
+counter = 0
+for k in range(0, 100000):
+    millis = time.time()
+    sum(list(filter(lambda x: (x % 2 == 0 and x < 4000000), ((lambda n: reduce(lambda y, _: y+[y[-1] + y[-2]], range(int(math.ceil(math.log10(n * math.sqrt(5)) / math.log10((1 + math.sqrt(5)) / 2))) - 2), [0, 1]))(4000000)))))
+    total_millis += (time.time() - millis)
+    counter += 1
+print(total_millis / counter)
