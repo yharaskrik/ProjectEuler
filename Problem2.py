164da5d2dd55b3d3d6b4ea40db76c84b6d85973c
yharaskrik <jaycooperbell@gmail.com>
2017-10-20 17:09:56 -0700

    Sped up Problem 2 by 60% by removing casting and changing position of if conditions to not be adding useless numbers.

--- Problem2.py
+++ Problem2.py
@@ -1,3 +1,9 @@
 from functools import reduce
 import math
-print(sum(list(filter(lambda x: (x % 2 == 0 and x < 4000000), ((lambda n: reduce(lambda y, _: y+[y[-1] + y[-2]], range(int(math.ceil(math.log10(n * math.sqrt(5)) / math.log10((1 + math.sqrt(5)) / 2))) - 2), [0, 1]))(4000000))))))
+import time
+millis = time.time()
+print(millis)
+print(sum(((lambda n: reduce(lambda y, _: y+[y[-1] + y[-2]] if (y[-1] + y[-2] % 2 == 0 and y[-1] + y[-2] < n) else y+[0], range(math.ceil(math.log10(n * math.sqrt(5)) / math.log10((1 + math.sqrt(5)) / 2)) - 2), [0, 1]))(4000000))))
+end = time.time()
+print(end)
+print(end - millis)
