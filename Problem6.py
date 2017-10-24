2cf659aea29e381d952f637635a6ae1b4249974a
yharaskrik <jaycooperbell@gmail.com>
2017-10-24 11:28:12 -0700

    Problem 7 and 8 solved

--- Problem6.py
+++ Problem6.py
@@ -4,7 +4,7 @@ sum_square = 0
 square_sum = 0
 
 for i in range(0, 101):
-    sum_square += math.pow(i, 2)
+    sum_square += i**2
     square_sum += i
 
-print(math.pow(square_sum, 2) - sum_square)
+print(square_sum**2 - sum_square)
